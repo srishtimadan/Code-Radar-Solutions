8105293dfbca890b1a632f2d83c62091589f6b06
srishtimadan <srishti.madan.oct20@gmail.com>
2025-04-11 13:37:51 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -20,7 +20,7 @@ int main(){
             printf("Fail");
             break ;
         default: 
-            printf("Invalid Grade");
+            printf("Invalid grade");
             break;
     }
     return 0;
