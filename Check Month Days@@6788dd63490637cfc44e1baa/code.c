955be61d018a78fb19febf8f317cf1f38f9fcf82
srishtimadan <srishti.madan.oct20@gmail.com>
2025-04-11 12:19:09 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -4,18 +4,18 @@ int main(){
     scanf("%d", &n);
 
     if (n >= 1 && n<= 12){
-        if (n == 1){printf("January");}
-        else if (n == 2){printf("February");}
-        else if (n == 3){printf("March");}
-        else if (n == 4){printf("April");}
-        else if (n == 5){printf("May");}
-        else if (n == 6){printf("June");}
-        else if (n == 7){printf("July");}
-        else if (n == 8){printf("August");}
-        else if (n == 9){printf("September");}
-        else if (n == 10){printf("October");}
-        else if (n == 11){printf("November");}
-        else if (n == 12){printf("December");}
+        if (n == 1){printf("31");}
+        else if (n == 2){printf("28");}
+        else if (n == 3){printf("31");}
+        else if (n == 4){printf("30");}
+        else if (n == 5){printf("31");}
+        else if (n == 6){printf("30");}
+        else if (n == 7){printf("31");}
+        else if (n == 8){printf("31");}
+        else if (n == 9){printf("30");}
+        else if (n == 10){printf("31");}
+        else if (n == 11){printf("30");}
+        else if (n == 12){printf("31");}
     }
     else {
         printf("Invalid");
